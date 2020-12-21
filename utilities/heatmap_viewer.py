97341905e5ddfb63b153c765b7ee13cad4471abd
StrayLamb2 <mpampinis@gmail.com>
2020-12-21 16:00:20 +0200

    a little more polishing, working on implementing heatmap in test_run

--- utilities/heatmap_viewer.py
+++ utilities/heatmap_viewer.py
@@ -13,11 +13,13 @@ parser.add_argument('output',metavar='output_dir',
                     help='Directory to save the heatmaps')
 args = parser.parse_args()
 
-print('Running with args: ')
+print('qLD Heatmap creator running with arguments:')
 print('\tinput:\t'+args.input)
 print('\toutput:\t'+args.output+'\n')
 
-for report in glob.glob(args.input+'*.txt'):
+ifile = os.path.splitext(args.input)[0]
+
+for report in glob.glob(args.input+'*'):
     if report.startswith('.'):
         continue
     print('Found '+report)
@@ -28,25 +30,20 @@ for report in glob.glob(args.input+'*.txt'):
     z = df['LD']
 
     df = df.set_index(['POS2','POS1'])
-    #print(df.index)
     df = df.unstack()
-    #df = df.fillna(0)
     df.columns = df.columns.get_level_values(1)
-    print(df)
 
-    mask = np.triu(np.ones_like(df, dtype=bool))
     n = 5
     color = plt.cm.get_cmap("tab20b_r", n)
 
     fig, ax = plt.subplots(figsize=(4, 3), dpi= 300)
-    fig.tight_layout()
+    plt.tight_layout()
     ax.autoscale()
     ax.tick_params(axis='both', which='major', labelsize=4)
     sns.heatmap(df, cmap=color, vmin=0, vmax=1, square=True, cbar_kws={"shrink": .8})
-    #sns.heatmap(df, mask=mask, cmap=color, vmin=0, vmax=1, square=True, cbar_kws={"shrink": .8})
     plt.xlabel('Position 1')
     plt.ylabel('Position 2')
     ax.set_title("LD scores\nin "+report, fontsize=11)
     plt.draw()
     plt.savefig(report.replace('.txt','.png'), bbox_inches='tight')
-print('Ended')
+print('Files saved in: '+os.path.dirname(args.input))
